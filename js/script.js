b71602b41923d5cacefb6f559e3554908f9f83c3
Diego Luis Mayolo <diego.mayolo@outlook.com>
2022-08-26 00:07:17 -0300

    animacoes de scroll na pagina; campos do form passam a ser required; criado menu hambrguer no header;

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+/**
+ * The function is called sendForm() and it is called when the user clicks the submit button.
+ */
 function sendForm() {
   // https://dashboard.emailjs.com/admin/account
   // Public Key = ITNG9TnXJG8kJ4Y9x
   emailjs.init('ITNG9TnXJG8kJ4Y9x');
 }
 
+/* Listening to the submit event of the form and preventing the default behavior of the event. */
 window.onload = function () {
   document
     .getElementById('contact-form')
@@ -37,6 +41,10 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
 //animar todos os itens da tela que tiverem o atributo [data-anime]
 const items = document.querySelectorAll('[data-anime]');
 
+/**
+ * If the top of the window is greater than the top of the element, add the class 'animate' to the
+ * element, otherwise remove the class 'animate' from the element.
+ */
 const animeScroll = () => {
   const windowTop = window.pageYOffset + window.innerHeight * 0.85;
 
@@ -52,3 +60,42 @@ const animeScroll = () => {
 window.addEventListener('scroll', () => {
   animeScroll();
 });
+
+// hamburguer function
+/* Open Menu bar */
+var iconMenu = document.querySelector('.iconmenu'),
+  iconOpen = document.querySelector('.iconopen'),
+  navBar = document.querySelector('.nav');
+
+iconMenu.addEventListener('click', openNav);
+
+function openNav() {
+  if (iconOpen.classList.contains('close')) {
+    iconOpen.classList.remove('close');
+  } else {
+    iconOpen.classList.add('close');
+  }
+
+  if (navBar.classList.contains('navshow')) {
+    navBar.classList.remove('navshow');
+  } else {
+    navBar.classList.add('navshow');
+  }
+}
+/* end Open Menu bar */
+
+/* Back to top */
+var toTop = document.getElementById('scrollme');
+
+toTop.addEventListener('click', function () {
+  scrollToTop(600);
+});
+
+function scrollToTop(scrollDuration) {
+  var scrollStep = -window.scrollY / (scrollDuration / 15),
+    scrollInterval = setInterval(function () {
+      if (window.scrollY != 0) {
+        window.scrollBy(0, scrollStep);
+      } else clearInterval(scrollInterval);
+    }, 15);
+}
